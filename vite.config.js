bbbcc76ec625277abd71d02c5a4abdcb49d2656a
EloChimombe <157219062+EloChimombe@users.noreply.github.com>
2024-05-08 14:19:37 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
   return {
-    base: '/WebPortfolio/',
+    base: '/portfolio/',
     plugins: [
       react(),
       // viteCompression()
     ],
     assetsInclude: ['**/*.jpg', '**/*.png', '**/*.svg', '**/*.gif'],
-    // base: '/Website Portfolio/'
   }
 });
